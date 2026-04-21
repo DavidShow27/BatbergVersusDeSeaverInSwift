a3569bfd31d3f11ec7cf298299ed8085e320404b
Diego Chavez <diego.chavez27@d155.org>
2026-04-21 07:37:45 -0500

    jdfjv

---
+++ BatbergVersusDeSeaver/Title Screen/StartGameView.swift
@@ -0,0 +1,18 @@
+//
+//  StartGameView.swift
+//  BatbergVersusDeSeaver
+//
+//  Created by DIEGO CHAVEZ on 4/21/26.
+//
+
+import SwiftUI
+
+struct StartGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartGameView()
+}
