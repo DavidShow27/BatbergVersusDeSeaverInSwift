ad43d5f086bc87369778f731a1acaff20d0b6317
David Show <david.show5c@gmail.com>
2026-04-21 18:06:37 -0500

    Change Player class to inherit from SKNode
    Refactor Player class to inherit from SKNode and add movement properties.

--- BatbergVersusDeSeaver/Entities/Player/Player.swift
+++ BatbergVersusDeSeaver/Entities/Player/Player.swift
@@ -8,17 +8,15 @@
 import Foundation
 import SpriteKit
 
-class Player {
+class Player: SKNode {
     
     // Stats
     var health: Int
     var ability: Bool
     
     // Movement
-    enum direction {
-        case left
-        case right
-    }
+    var xSpeed: CGFloat
+    var ySpeed: CGFloat
     var jumping: Bool
     var groundPound: Bool
     var sliding: Bool
@@ -35,7 +33,19 @@ class Player {
         self.groundPound = false
         self.sliding = false
         self.crouch = false
-        
+        self.xSpeed = 0
+        self.ySpeed = 0
         self.sprite = SKSpriteNode(imageNamed: "BatbergPlaceHolder")
+        
+        // Get stuff from SKNode
+        super.init()
+        
+        sprite.size = CGSize(width: 300, height: 300)
+        // Add the sprite to a GameScene or any .sks file
+        addChild(sprite)
+    }
+    // This is only for the SpriteKit because it is Objective-C
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
