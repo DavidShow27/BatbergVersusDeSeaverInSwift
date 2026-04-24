0e93a9557d8fbdf8c00c93373fcb64b6d446d1f5
Diego Chavez <diego.chavez27@d155.org>
2026-04-24 07:41:00 -0500

    fyeduyb

---
+++ BatbergVersusDeSeaver/App/GameViewControllerRepresentable.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewControllerRepresentable.swift
+//  BatbergVersusDeSeaver
+//
+//  Created by DIEGO CHAVEZ on 4/23/26.
+//
+
+import SwiftUI
+
+struct GameViewControllerRepresentable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameViewControllerRepresentable()
+}
