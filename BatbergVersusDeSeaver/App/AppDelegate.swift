ba00d9d1b8942387ad87aab0cc3f5f5ec19f15ec
diego.chavez27 <noicedude555@gmail.com>
2026-04-27 07:51:19 -0500

    finally

--- BatbergVersusDeSeaver/App/AppDelegate.swift
+++ BatbergVersusDeSeaver/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIHostingController(rootView: TitleView())
+        window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
     }
