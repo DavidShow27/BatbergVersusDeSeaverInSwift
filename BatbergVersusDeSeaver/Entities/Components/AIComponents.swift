f4eff386b9f68b07cafc5dfd14424ff45f7321dd
David Show <david.show5c@gmail.com>
2026-04-27 08:50:21 -0500

    Follow entity is getting there

--- BatbergVersusDeSeaver/Entities/Components/AIComponents.swift
+++ BatbergVersusDeSeaver/Entities/Components/AIComponents.swift
@@ -5,3 +5,29 @@
 //  Created by DAVID SHOW on 4/22/26.
 //
 
+import Foundation
+import GameplayKit
+
+class FollowEntity : GKComponent {
+    
+    var sprite: SpriteComponent? {
+        return entity?.component(ofType: SpriteComponent.self)
+    }
+    
+    var move: MovementComponent? {
+        return entity?.component(ofType: MovementComponent.self)
+    }
+    
+    var actions = GKAgent()
+    
+    func followEntity(who: GKEntity) {
+        
+        guard let whoPosition = who.component(ofType: SpriteComponent.self)?.node.position else { return }
+        guard let selfPosition = sprite?.node.position else { return }
+        let direction: CGPoint? = CGPoint(x: selfPosition.x - whoPosition.x, y: selfPosition.y - whoPosition.y)
+        //let vector: CGVector? = CGVector(dx: move?.velocity.dx.magnitude * direction.x, dy: move?.velocity.dy.magnitude * direction?.y)
+        
+        
+    }
+    
+}
