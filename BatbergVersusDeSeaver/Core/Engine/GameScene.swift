73f520faeeb743be2e603d41634dc8e86a153671
Diego Chavez <diego.chavez27@d155.org>
2026-04-29 08:01:46 -0500

    eub

--- BatbergVersusDeSeaver/Core/Engine/GameScene.swift
+++ BatbergVersusDeSeaver/Core/Engine/GameScene.swift
@@ -5,43 +5,62 @@
 //  Created by DAVID SHOW on 4/16/26.
 //
 
-import SpriteKit
 import GameplayKit
+import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    
+
     var wallImage = ""
     var floorImage = ""
-    
+
     var floor1 = SKSpriteNode()
     var wall1R = SKSpriteNode()
     var wall1L = SKSpriteNode()
-    
+
     var player = Player.shared
-    
+    var grapple: Grapple!
+    var grappleSprite: SKSpriteNode!
+
     let cam = SKCameraNode()
-    
+
     let joyStick = Joystick(size: 100)
     let actionButton = ActionButton(size: CGSize(width: 175, height: 175))
-    
+
     //on scene load
     override func didMove(to view: SKView) {
-        
+
         //addChild(makeFLoor(size: CGSize(width: 200, height: 20), position: CGPoint(x: 50, y: -50)))
-        
+
         physicsWorld.contactDelegate = self
-        
+
         wall1R = SKSpriteNode(imageNamed: wallImage)
-        
-        player.component(ofType: SpriteComponent.self)?.node
-        = self.childNode(withName: "player") as! SKSpriteNode
+
+        player.component(ofType: SpriteComponent.self)?.node =
+            self.childNode(withName: "player") as! SKSpriteNode
+        player.addComponent(grapple)
+        if let sprite = Player.shared.component(ofType: SpriteComponent.self)?
+            .node
+        {
+            let position = sprite.position
+            grappleSprite = SKSpriteNode(imageNamed: "grapple")
+            grappleSprite.position = position
+            grappleSprite.isHidden = true
+
+            grapple = Grapple(
+                velocity: .zero,
+                playerPosition: position,
+                ground: 100
+            )
+
+        }
+        addChild(grappleSprite)
 
         addChild(cam)
         self.camera = cam
-        
+
         addChild(joyStick)
         addChild(actionButton)
-        
+
         joyStick.onCrouchChanged = { isCrouching in
             if isCrouching {
                 self.actionButton.label.text = "slide"
@@ -49,34 +68,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 self.actionButton.label.text = "jump"
             }
         }
-        
+
     }
-    
+
     //before each frame
+    
     override func update(_ currentTime: TimeInterval) {
-        player.update(deltaTime: 1/60)
-        
-        guard let xPos = player.component(ofType: SpriteComponent.self)?.node.position.x else { return }
-        guard let yPos = player.component(ofType: SpriteComponent.self)?.node.position.y else { return }
-        
+        player.update(deltaTime: 1 / 60)
+
+        guard
+            let xPos = player.component(ofType: SpriteComponent.self)?.node
+                .position.x
+        else { return }
+        guard
+            let yPos = player.component(ofType: SpriteComponent.self)?.node
+                .position.y
+        else { return }
+
         cam.position.x = xPos
         cam.position.y = yPos + 100
-        
+
         joyStick.position.x = xPos - (size.width / 3)
         joyStick.position.y = yPos - (size.height / 10)
-        
+
         actionButton.position.x = xPos + (size.width / 3)
         actionButton.position.y = yPos - (size.height / 10)
-        
+
     }
-    
+
     // Contact
     func didBegin(_ contact: SKPhysicsContact) {
         player.component(ofType: JumpComponent.self)?.isJumping = false
-        player.component(ofType: GroundPoundComponent.self)?.isGroundPounding = false
+        player.component(ofType: GroundPoundComponent.self)?.isGroundPounding =
+            false
     }
-    
-    func makeFLoor(size: CGSize, position: CGPoint) -> SKSpriteNode{
+
+    func makeFLoor(size: CGSize, position: CGPoint) -> SKSpriteNode {
         floor1 = SKSpriteNode(imageNamed: floorImage)
         floor1.size = size
         floor1.position = position
@@ -88,4 +115,3 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return floor1
     }
 }
-
