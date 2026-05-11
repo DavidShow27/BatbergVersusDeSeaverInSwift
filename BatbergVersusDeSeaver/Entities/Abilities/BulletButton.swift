887f10c7463073bd94d6bcaf24791118409ae6f5
FuegoDiego <noicedude555@gmail.com>
2026-05-11 16:46:04 -0500

    Update BulletButton.swift

--- BatbergVersusDeSeaver/Entities/Abilities/BulletButton.swift
+++ BatbergVersusDeSeaver/Entities/Abilities/BulletButton.swift
@@ -1,5 +1,41 @@
-import foundation
+import Foundation
+import GameplayKit
 
-class BulletButton: SKNode {
-  
+class ActionButton: SKNode {
+
+    var player = Player.shared
+    
+    var label: SKLabelNode
+    private var backGround: SKShapeNode
+
+    init(size: CGSize) {
+        backGround = SKShapeNode(rectOf: size, cornerRadius: 25)
+        backGround.fillColor = .clear
+        backGround.strokeColor = .white
+        backGround.lineWidth = 2
+        label = SKLabelNode(text: "shoot")
+        label.fontName = "MortalKombat-Regular"
+        label.fontSize = 48
+        label.fontColor = .white
+        super.init()
+        isUserInteractionEnabled = true
+        addChild(backGround)
+        addChild(label)
+        label.position.y = backGround.frame.midY
+    }
+
+    required init?(coder: NSCoder) {
+        backGround = SKShapeNode(rectOf: CGSize(width: 100, height: 100), cornerRadius: 25)
+        backGround.fillColor = .clear
+        backGround.strokeColor = .white
+        backGround.lineWidth = 10
+        label = SKLabelNode(text: "shoot")
+        label.fontName = "MortalKombat-Regular"
+        label.fontSize = 48
+        label.fontColor = .white
+        super.init(coder: coder)
+        isUserInteractionEnabled = true
+        addChild(backGround)
+        addChild(label)
+    }
 }
