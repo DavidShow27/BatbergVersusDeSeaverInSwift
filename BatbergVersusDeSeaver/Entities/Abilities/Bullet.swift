1cdee6885c3fe9b0e02a91cb469fcafd12a58527
FuegoDiego <noicedude555@gmail.com>
2026-05-10 22:39:18 -0500

    Update Bullet.swift

--- BatbergVersusDeSeaver/Entities/Abilities/Bullet.swift
+++ BatbergVersusDeSeaver/Entities/Abilities/Bullet.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class Bullet: GKComponent {
+    var bull: SKSpriteNode?
+
+    override func didAddToEntity() {
+
+        if let playerNode = entity?.component(ofType: SpriteComponent.self)?
+            .node
+        {
+            bull = SKSpriteNode(imageNamed: "hook")
+            bull?.position = playerNode.position
+            bull?.setScale(0.1)
+            bull?.zPosition = 0
+
+            bull?.physicsBody = SKPhysicsBody(circleOfRadius: bull!.size.width / 2)
+            
+            bull?.physicsBody?.affectedByGravity = false
+            bull?.physicsBody?.isDynamic = true
+            
+            bull?.physicsBody?.categoryBitMask = PhysicsCategory.bullet
+            bull?.physicsBody?.contactTestBitMask =
+            PhysicsCategory.floor | PhysicsCategory.enemy | PhysicsCategory.player
+            bull?.physicsBody?.collisionBitMask =
+            PhysicsCategory.floor | PhysicsCategory.enemy | PhysicsCategory.player
+            bull?.physicsBody?.allowsRotation = false
+            
+            bull?.name = "bullet"
+        }
+
+    }
+}
